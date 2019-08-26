226d152a35fca2e1a8546282912a3e0b369fc573
Carlos <cortega@vailsys.com>
2019-08-26 00:12:13 -0500

    CH-10

--- app/assets/javascripts/populations.js
+++ app/assets/javascripts/populations.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+window.onload = function () {
+  input = document.getElementById('year-form')
+  input.addEventListener("input", function(event) {
+    value = event.target.value
+    radios = document.getElementById('radios')
+
+    if (value > 1990) {
+      radios.style.display = 'block'
+    }
+    else{
+      radios.style.display = 'none'
+    }
+  })
+}
+
